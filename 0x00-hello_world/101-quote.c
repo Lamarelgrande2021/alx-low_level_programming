a0cecf8f98d1cb10ed2eb4476c5b999d3df50445
Lamarel <adeniyiidris94@gmail.com>
2023-02-16 09:29:52 -0800

    101-quote.c correct

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio>
 #include <unistd.h>
 /**
- * main - A program of that prints a line to the standard error
+ * main - A program of c that prints a line to the standard error
  * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
